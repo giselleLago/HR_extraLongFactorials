546a9cdd76e2a72fdba7566078596481ea1c7090
giselle <gisellelago25994@gmail.com>
2019-12-09 20:04:21 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Solution
+{
+
+    // Complete the extraLongFactorials function below.
+    static BigInteger extraLongFactorials(int n)
+    {
+        int num = n - 1;
+        long resFinal = n;
+        var resultFinal = new BigInteger(resFinal);
+
+        while (num > 0)
+        {
+            resultFinal = resultFinal * num;
+            n--;
+            num--;
+        }
+        return resultFinal;
+        
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+
+        var result = extraLongFactorials(n);
+
+
+        Console.WriteLine(result);
+        Console.ReadKey();
+
+    }
+}
